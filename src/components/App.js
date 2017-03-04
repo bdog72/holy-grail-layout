617cc39a6832b42912e75e6efd0062ec8e3ed910
Brian Bycynski <high5bri@hotmail.com>
2017-03-04 07:02:23 -0500

    finished basic layout

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react'
 class App extends Component {
   render () {
     return <div className='grid'>
-      <header>Header</header>
-      <aside className='sidebar-right'>right sidebar</aside>
-      <article>article</article>
-      <aside className='sidebar-left'>left sidebar</aside>
-      <footer>footer</footer>
+      <header><h1>Header</h1></header>
+      <aside className='sidebar-right'><h1>R Sidebar</h1></aside>
+      <article><h1>Article</h1></article>
+      <aside className='sidebar-left'><h1>L Sidebar</h1></aside>
+      <footer><h1>Footer</h1></footer>
     </div>
   }
 }
